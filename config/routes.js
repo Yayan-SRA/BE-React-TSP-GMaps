31b55602920fa6b3ba12b50801181fac9187e068
Yayan-SRA <yayansra14@gmail.com>
2022-10-31 16:02:18 +0700

    fix

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const controllers = require("../app/controllers");
 
-const apiRouter = express.Router();
+const router = express.Router();
 
 /**
  * TODO: Implement your own API
  *       implementations
  */
-apiRouter.get("/api/v1/algen/list", controllers.api.v1.algenController.list);
-apiRouter.post("/api/v1/algen/create", controllers.api.v1.algenController.create);
-apiRouter.put("/api/v1/algen/update/:id", controllers.api.v1.algenController.update);
-apiRouter.get("/api/v1/algen/show/:id", controllers.api.v1.algenController.show);
-apiRouter.delete(
+router.get("/api/v1/algen/list", controllers.api.v1.algenController.list);
+router.post("/api/v1/algen/create", controllers.api.v1.algenController.create);
+router.put("/api/v1/algen/update/:id", controllers.api.v1.algenController.update);
+router.get("/api/v1/algen/show/:id", controllers.api.v1.algenController.show);
+router.delete(
   "/api/v1/algen/delete/:id",
   controllers.api.v1.algenController.destroy
 );
@@ -21,13 +21,13 @@ router.get('/', controllers.api.v1.algenController.handleGetRoot)
  * TODO: Delete this, this is just a demonstration of
  *       error handler
  */
-apiRouter.get("/api/v1/errors", () => {
+router.get("/api/v1/errors", () => {
   throw new Error(
     "The Industrial Revolution and its consequences have been a disaster for the human race."
   );
 });
 
-apiRouter.use(controllers.api.main.onLost);
-apiRouter.use(controllers.api.main.onError);
+router.use(controllers.api.main.onLost);
+router.use(controllers.api.main.onError);
 
-module.exports = apiRouter;
+module.exports = router;
